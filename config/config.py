2172bf5f897d8bfb264f8e4b239797ffcc1bd67a
melih00can10 <92529973+melih00can10@users.noreply.github.com>
2022-07-14 01:08:10 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/melih00can10/tgbelediye",
+    "https://github.com/melih00can10/tgbelediyepro",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
